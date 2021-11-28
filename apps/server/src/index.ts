850e80dc0e5521976b4a9d0687b37a5c665a88d8
Sam Bigelow <sambigelow44@gmail.com>
2021-11-28 10:00:30 -0500

    Run prettier against all code

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,13 +4,13 @@ const address = 3000
 const fastify = Fastify()
 
 fastify.get('/ping', async (_request, _reply) => {
-  return 'pong\n'
+	return 'pong\n'
 })
 
 fastify.listen(address, (err: any) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
+	if (err) {
+		console.error(err)
+		process.exit(1)
+	}
+	console.log(`Server listening at ${address}`)
 })
