28304e610b8abb84fd2c2fa4729a02106a19e6c5
Sam Bigelow <sambigelow44@gmail.com>
2021-11-28 16:17:39 -0500

    WIP

---
+++ apps/web/src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+	import { createClient } from '@urql/svelte'
+
+	const client = createClient({
+		url: 'http://localhost:8080/graphql'
+	})
+</script>
+
+<script>
+	import { setClient } from '@urql/svelte'
+
+	setClient(client)
+</script>
+
+<slot />
